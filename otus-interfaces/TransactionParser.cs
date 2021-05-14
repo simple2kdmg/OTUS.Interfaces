9cf588eaab68b022331201b7611c7ee466e35f03
Almaz Siraev <Almaz.Siraev@icl-services.com>
2021-05-15 00:26:32 +0300

    Added new things

---
+++ otus-interfaces/TransactionParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace otus_interfaces
+{
+    public class TransactionParser : ITransactionParser
+    {
+        public ITransaction Parse(string input)
+        {
+            var date = DateTimeOffset.Now;
+            var splits = input.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            var typeCode = splits[0];
+            var currencyAmount = new CurrencyAmount(splits[2], decimal.Parse(splits[1]));
+            switch (typeCode)
+            {
+                case "Трата":
+                    return new Expense(currencyAmount, date, splits[3], splits[4]);
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
